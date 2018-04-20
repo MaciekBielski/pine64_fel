063416adb9a1dcf6be8f64d423c41af00b741cba
Maciej Bielski <bielski@fastmail.net>
2018-04-20 23:26:58 +0200

    trying to reset to aarch64

--- test_kernel/startup.s
+++ test_kernel/startup.s
@@ -17,9 +17,11 @@ _reset:
 
 uart0_thr:	.word	0x01C28000
 uart0_lsr:	.word	0x01C28014
+c_ctrl_reg0:	.word	0x01700c00
 
+pre:	.word	0x77767574
 txt:
-	.string	"Test\n"
+	.word	0x00000000, 0x74757677 /* c0cad00d\n */
 	.align	3
 
 
@@ -34,7 +36,7 @@ _loadc:
 	/* Load character */
 	ldrb	r7, [r5, r6]
 	/* Compare with NULL */
-	cmp	r7, #0
+	cmp	r6, #12
 	bne	_putc
 	mov	pc, lr
 _putc:
@@ -46,8 +48,19 @@ _putc:
 
 /* CPU starts in 32bit mode, a code here needs to reset it to aarch64 */
 reset_handler:
-	ldr	r0, =txt
+	ldr	r0, =pre
+	ldr	r3, =txt
+
+	ldr	r2, =c_ctrl_reg0
+	ldr	r2, [r2]
+
+	ldr	r4, [r2]
+	orr	r4, r4, #0x51000000
+	str	r4, [r2]
+
+	ldr	r1, [r2]
+	str	r1, [r3]
 	bl	puts
+
+	/* mrc	p15,0,r2,c1,c0,2 */
 	b	.
-	/* read RMR */
-	/* mrc p15,0,r0,c12,c0,2 */
